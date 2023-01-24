cbfb97ac737d53fddfc1689e9f9e4b3214b92801
HFZ1928 <fzhanif929@gmail.com>
2023-01-24 21:12:09 +0700

    master: backend membuat model get dan delete

---
+++ Server/application/models/Siswa_model.php
@@ -0,0 +1,25 @@
+<?php
+
+class Siswa_model extends CI_Model {
+
+
+    public function getSiswa($id = null){ // membuat fungsi model tampil / lihat data dari database
+        
+        if ( $id == null){ // kondisi jika id tidak di masukkan maka tampilkan seluruh data
+            return $this->db->get('siswa')->result_array();
+        }else{// kondisi jika user memasukkan id yang dicari, maka akan muncul id tersebut yang dicari
+            return $this->db->get_where('siswa',['id' => $id])->result_array();    
+        }
+    }
+
+
+
+    public function deleteSiswa($id){ // membuat fungsi delete siswa by id
+
+        $this->db->delete('siswa', ['id' => $id]); 
+        return $this->db->affected_rows();
+    }
+
+
+
+}
